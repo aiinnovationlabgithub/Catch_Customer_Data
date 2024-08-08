3b4a7488b03693246d87e6d401655ef3a13b5fbd
aiinnovationlabgithub <ai.innovation.lab.gmo@gmail.com>
2024-08-08 13:47:52 +0800

    2024/08/08

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ db_settings = {
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
 
